1d4c10389a63cb0911c035abab93d247c5114885
dummy2004 <p.klee.4bdb@gmail.com>
2023-11-24 14:25:43 -0600

    ejercicio 3 hecho

--- PAT_Parcial03/Ejercicio03.cpp
+++ PAT_Parcial03/Ejercicio03.cpp
@@ -1,4 +1,7 @@
+// Ejercicio03.cpp
+
 #include "Ejercicio03.h"
+#include <algorithm>
 
 TimeMap::TimeMap()
 {
@@ -6,9 +9,34 @@ TimeMap::TimeMap()
 
 void TimeMap::set(string key, string value, int timestamp)
 {
+    auto it = data.find(key);
+    if (it == data.end()) {
+        data[key] = { {timestamp, value} };
+    }
+    else {
+        it->second.push_back({ timestamp, value });
+    }
 }
 
 string TimeMap::get(string key, int timestamp)
 {
-	return "";
+    auto it = data.find(key);
+    if (it != data.end()) {
+
+        const auto& timestamps = it->second;
+
+        auto it_time = std::lower_bound(timestamps.begin(), timestamps.end(), timestamp,
+            [](const pair<int, string>& a, int ts) {
+                return a.first < ts;
+            });
+
+        if (it_time != timestamps.begin()) {
+
+            size_t index = std::distance(timestamps.begin(), it_time - 1);
+            return timestamps[index].second;
+        }
+    }
+
+    return "";
 }
+
