7e7822f9348eda299e86736ecd53a7be3e4f4e95
dummy2004 <p.klee.4bdb@gmail.com>
2023-11-24 14:12:21 -0600

    primer ejecircio hecho

--- PAT_Parcial03/Ejercicio01.cpp
+++ PAT_Parcial03/Ejercicio01.cpp
@@ -1,6 +1,35 @@
 #include "Ejercicio01.h"
+#include <queue>
 
-Node<int>* Ejercicio01::mergeLists(Node<Node<int>*>* lists)
-{
-    return nullptr;
+Node<int>* Ejercicio01::mergeLists(Node<Node<int>*>* lists) {
+    auto comparator = [](Node<int>* a, Node<int>* b) {
+        return a->value < b->value;
+        };
+
+    std::priority_queue<Node<int>*, std::vector<Node<int>*>, decltype(comparator)> pq(comparator);
+
+    while (lists != nullptr) {
+        if (lists->value != nullptr) {
+            pq.push(lists->value);
+        }
+        lists = lists->next;
+    }
+
+    Node<int>* dummy = new Node<int>{ 0, nullptr }; 
+    Node<int>* current = dummy;
+
+    while (!pq.empty()) {
+        Node<int>* top = pq.top();
+        pq.pop();
+
+        current->next = top;
+        current = current->next;
+
+        if (top->next != nullptr) {
+            pq.push(top->next);
+        }
+    }
+
+    return dummy->next;
 }
+
