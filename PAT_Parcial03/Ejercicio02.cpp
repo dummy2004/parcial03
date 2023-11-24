5e3c60dc52d4ad3caedb4f82d021c8b94278b7f5
dummy2004 <p.klee.4bdb@gmail.com>
2023-11-24 14:15:15 -0600

    ejercicio 2 hecho, falta hacer las pruebas

--- PAT_Parcial03/Ejercicio02.cpp
+++ PAT_Parcial03/Ejercicio02.cpp
@@ -1,6 +1,34 @@
 #include "Ejercicio02.h"
+#include <unordered_map>
 
 vector<string>* Ejercicio02::findRepeatedDnaSequences(Node<char>* head)
 {
-    return nullptr;
+    std::unordered_map<string, int> sequencesMap;
+
+    vector<string>* result = new vector<string>();
+
+    string currentSequence;
+
+    while (head != nullptr) {
+        currentSequence.push_back(head->value);
+
+        if (currentSequence.length() > 10) {
+            currentSequence.erase(currentSequence.begin());
+        }
+
+        if (currentSequence.length() == 10) {
+            sequencesMap[currentSequence]++;
+        }
+
+        head = head->next;
+    }
+
+    for (const auto& entry : sequencesMap) {
+        if (entry.second > 1) {
+            result->push_back(entry.first);
+        }
+    }
+
+    return result;
 }
+
