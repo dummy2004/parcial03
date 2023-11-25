e5d67fa4bf294d077871396567acd45eb05f46cd
dummy2004 <p.klee.4bdb@gmail.com>
2023-11-25 08:58:45 -0600

    no corre las pruebas, todavia falta la partre teorica y proyecto

--- PAT_Parcial03/Ejercicio03.cpp
+++ PAT_Parcial03/Ejercicio03.cpp
@@ -1,5 +1,3 @@
-// Ejercicio03.cpp
-
 #include "Ejercicio03.h"
 #include <algorithm>
 
@@ -11,7 +9,7 @@ void TimeMap::set(string key, string value, int timestamp)
 {
     auto it = data.find(key);
     if (it == data.end()) {
-        data[key] = { {timestamp, value} };
+        data[key] = vector<pair<int, string>>{ {timestamp, value} };
     }
     else {
         it->second.push_back({ timestamp, value });
@@ -22,16 +20,13 @@ string TimeMap::get(string key, int timestamp)
 {
     auto it = data.find(key);
     if (it != data.end()) {
-
         const auto& timestamps = it->second;
-
         auto it_time = std::lower_bound(timestamps.begin(), timestamps.end(), timestamp,
             [](const pair<int, string>& a, int ts) {
                 return a.first < ts;
             });
 
         if (it_time != timestamps.begin()) {
-
             size_t index = std::distance(timestamps.begin(), it_time - 1);
             return timestamps[index].second;
         }
@@ -39,4 +34,3 @@ string TimeMap::get(string key, int timestamp)
 
     return "";
 }
-
